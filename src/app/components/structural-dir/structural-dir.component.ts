fe2f43bbef62ae66c3e1daad6e44a2cf3e617375
Amanavi75 <amanavi75@gmail.com>
2024-08-29 17:26:56 +0530

    done with ngIf and ngFor

--- src/app/components/structural-dir/structural-dir.component.ts
+++ src/app/components/structural-dir/structural-dir.component.ts
@@ -13,9 +13,29 @@ export class StructuralDirComponent {
 
   num1 :string = ""
   num2:string=""
-
-
   isDivisible:boolean = true;
+  cityArray:string [] = ['oune', 'delhi','patna','adi']
+
+  studentList:any []= [
+    {
+      id:1,
+      name:"ama",
+      city:"delh",
+      isActive:false
+    },
+    {
+      id:2,
+      name:"am",
+      city:"del",
+      isActive:true
+    },
+    {
+      id:3,
+      name:"a",
+      city:"de",
+      isActive:false
+    },
+  ]
   showDiv1(){
     this.isDivisible = true ;
   }
@@ -26,3 +46,6 @@ export class StructuralDirComponent {
   
 
 }
+
+// we can apply *ngFor on any type of element 
+// mostly  we will use this in case of drop down 
