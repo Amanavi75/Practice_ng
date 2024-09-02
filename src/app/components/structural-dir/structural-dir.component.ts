20c692b65fd55b042a470e9069118104e21048bd
Amanavi75 <amanavi75@gmail.com>
2024-09-02 11:01:18 +0530

    additional method to add router using html and typescript function

--- src/app/components/structural-dir/structural-dir.component.ts
+++ src/app/components/structural-dir/structural-dir.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-structural-dir',
@@ -15,6 +16,8 @@ export class StructuralDirComponent {
   num2:string=""
   isDivisible:boolean = true;
   cityArray:string [] = ['oune', 'delhi','patna','adi']
+
+
 
   studentList:any []= [
     {
@@ -36,6 +39,17 @@ export class StructuralDirComponent {
       isActive:false
     },
   ]
+
+  //creating object of injectable services 
+  //way 1 - through constructor (dependency injection)
+  
+  constructor(private router:Router){
+
+  }
+  navigateToattrbutive(){
+    this.router.navigateByUrl("attributaldirective")
+
+  }
   showDiv1(){
     this.isDivisible = true ;
   }
