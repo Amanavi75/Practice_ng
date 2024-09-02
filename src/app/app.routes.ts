467373393f281e5e382ad25a81844dda2f580456
Amanavi75 <amanavi75@gmail.com>
2024-09-02 09:33:58 +0530

    started with the routing process

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { EmpComponent } from './components/emp/emp.component';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { StructuralDirComponent } from './components/structural-dir/structural-dir.component';
+import { AttributedirComponent } from './components/attributedir/attributedir.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"add-emp",
+        component:EmpComponent
+
+    },
+    {
+        path:'data-binding',
+        component:DataBindingComponent
+
+    },
+    {
+        path:"structural directive",
+        component:StructuralDirComponent
+    },
+
+    {
+        path:"attributal directive",
+        component:AttributedirComponent
+    }
+];
