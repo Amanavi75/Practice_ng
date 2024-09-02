cfcd6304fbe5c038e1cb5a6489036ea2f89570d4
Amanavi75 <amanavi75@gmail.com>
2024-09-02 12:23:04 +0530

    added control flow working architecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { StructuralDirComponent } from './components/structural-dir/structural-dir.component';
 import { AttributedirComponent } from './components/attributedir/attributedir.component';
+import { IfelseComponent } from "./components/ifelse/ifelse.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet, RouterLink, IfelseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
